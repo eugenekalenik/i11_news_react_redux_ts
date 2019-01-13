b46e843b69ede51245efaa8619bd56b435a95743
Eugene Kalenik <6361645@gmail.com>
2019-01-13 23:39:57 +0300

    Updates

---
+++ precache-manifest.6718d7db69fc789fffe35104e480aab7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e5a33f2c924be1a4a64",
+    "url": "/i11_news_react_redux_ts/static/js/runtime~main.2e5a33f2.js"
+  },
+  {
+    "revision": "5480ef115ec048bf9c84",
+    "url": "/i11_news_react_redux_ts/static/js/main.5480ef11.chunk.js"
+  },
+  {
+    "revision": "cca58def9a5194630ff5",
+    "url": "/i11_news_react_redux_ts/static/js/1.cca58def.chunk.js"
+  },
+  {
+    "revision": "5480ef115ec048bf9c84",
+    "url": "/i11_news_react_redux_ts/static/css/main.7a5ecb97.chunk.css"
+  },
+  {
+    "revision": "ddba38c8aa0ca107f8b90f77eba1af1b",
+    "url": "/i11_news_react_redux_ts/index.html"
+  }
+];
